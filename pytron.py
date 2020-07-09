1db29004807eb24fffeb44b65ee6cf085bce4187
Or <orkop465@gmail.com>
2020-07-10 02:48:11 +0300

    Added tron-style movement, and walls

--- pytron.py
+++ pytron.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-003_static_blit_pretty.py
-static blitting and drawing (pretty version)
-url: http://thepythongamebook.com/en:part2:pygame:step003
-author: horst.jens@spielend-programmieren.at
-licence: gpl, see http://www.gnu.org/licenses/gpl.html
 
-works with pyhton3.4 and python2.7
+TODO:
+    FIRST: SPLIT INTO GAME AND PLAYER CLASSES   DONE :)
+    Change controls to snake-style              DONE :)
+    Add dying (from walls / trail)              next
+    Tron style trail
 
-Blitting a surface on a static position
-Drawing a filled circle into ballsurface.
-Blitting this surface once.
-introducing pygame draw methods
-The ball's rectangular surface is black because the background
-color of the ball's surface was never defined nor filled."""
+TODO late-game:
+    Add sprites
+    Multiple players / AI
+
+"""
+import itertools
 
 import pygame
 
 
-class PygView(object):
+class Game(object):
 
     def __init__(self, width=640, height=400, fps=30):
         """Initialize pygame, window, background, font,...
@@ -30,28 +29,51 @@ class PygView(object):
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
         self.background = pygame.Surface(self.screen.get_size()).convert()
-        self.background.fill((255, 255, 255))  # fill background white
+        self.background.fill((0, 0, 0))  # fill background white
         self.clock = pygame.time.Clock()
         self.fps = fps
         self.playtime = 0.0
-        self.font = pygame.font.SysFont('mono', 24, bold=True)
+        self.players = [Player("red", int((self.width / 2) - 25), self.height - 25, 5)]
+
+        self.colors = itertools.cycle(['green', 'blue', 'purple', 'pink', 'red', 'orange'])
+        self.base_color = next(self.colors)
+        self.next_color = next(self.colors)
+        self.current_color = self.base_color
+        self.change_every_x_seconds = 3.
+        self.number_of_steps = self.change_every_x_seconds * self.fps
+        self.step = 1
 
     def paint(self):
         """painting on the surface"""
-        # ------- try out some pygame draw functions --------
-        # pygame.draw.line(Surface, color, start, end, width)
-        pygame.draw.line(self.background, (0, 255, 0), (10, 10), (50, 100))
-        # pygame.draw.rect(Surface, color, Rect, width=0): return Rect
-        pygame.draw.rect(self.background, (0, 255, 0), (50, 50, 100, 25))  # rect: (x1, y1, width, height)
-        # pygame.draw.circle(Surface, color, pos, radius, width=0): return Rect
-        pygame.draw.circle(self.background, (0, 200, 0), (200, 50), 35)
-        # pygame.draw.polygon(Surface, color, pointlist, width=0): return Rect
-        pygame.draw.polygon(self.background, (0, 180, 0), ((250, 100), (300, 0), (350, 50)))
-        # pygame.draw.arc(Surface, color, Rect, start_angle, stop_angle, width=1): return Rect
-        pygame.draw.arc(self.background, (0, 150, 0), (400, 10, 150, 100), 0, 3.14)  # radiant instead of grad
-        # ------------------- blitting a Ball --------------
-        myball = Ball()  # creating the Ball object
-        myball.blit(self.background)  # blitting it
+        # PLAYERS
+        for player in self.players:
+            pygame.draw.rect(self.background, (255, 0, 0),
+                             (player.x, player.y, 20, 20))  # rect: (x1, y1, width, height)
+        # WALLS
+        self.color_shifter()
+        # left
+        pygame.draw.rect(self.background, self.current_color, (0, 0, 5, self.height))
+        # bottom
+        pygame.draw.rect(self.background, self.current_color,
+                         (0, self.height - 5, self.width, 5))
+        # right
+        pygame.draw.rect(self.background, self.current_color,
+                         (self.width - 5, 0, 5, self.height))
+        # top
+        pygame.draw.rect(self.background, self.current_color, (0, 0, self.width, 5))
+
+    def color_shifter(self):
+        self.step += 1
+        if self.step < self.number_of_steps:
+            # (y-x)/number_of_steps calculates the amount of change per step required to
+            # fade one channel of the old color to the new color
+            # We multiply it with the current step counter
+            self.current_color = [x + (((y - x) / self.number_of_steps) * self.step) for x, y in
+                                  zip(pygame.color.Color(self.base_color), pygame.color.Color(self.next_color))]
+        else:
+            self.step = 1
+            self.base_color = self.next_color
+            self.next_color = next(self.colors)
 
     def run(self):
         """The mainloop
@@ -66,49 +88,57 @@ class PygView(object):
                     if event.key == pygame.K_ESCAPE:
                         running = False
 
+            keys = pygame.key.get_pressed()
+
+            # Making sure the top left position of our character is greater than our vel so we never move off the screen
+            if keys[pygame.K_LEFT] and self.players[0].direction != 'R':
+                self.players[0].direction = 'L'
+
+            # Making sure the top right corner of our character is less than the screen width - its width
+            if keys[pygame.K_RIGHT] and self.players[0].direction != 'L':
+                self.players[0].direction = 'R'
+
+            # Same principles apply for the y coordinate
+            if keys[pygame.K_UP] and self.players[0].direction != 'D':
+                self.players[0].direction = 'U'
+
+            if keys[pygame.K_DOWN] and self.players[0].direction != 'U':
+                self.players[0].direction = 'D'
+
+            self.players[0].move()
+
+            self.background.fill((0, 0, 0))
+            self.paint()
+
             milliseconds = self.clock.tick(self.fps)
             self.playtime += milliseconds / 1000.0
-            self.draw_text("FPS: {:6.3}{}PLAYTIME: {:6.3} SECONDS".format(
-                self.clock.get_fps(), " " * 5, self.playtime))
 
             pygame.display.flip()
             self.screen.blit(self.background, (0, 0))
 
         pygame.quit()
 
-    def draw_text(self, text):
-        """Center text in window
-        """
-        fw, fh = self.font.size(text)
-        surface = self.font.render(text, True, (0, 0, 0))
-        self.screen.blit(surface, (50, 150))
 
+class Player:
 
-class Ball(object):
-    """this is not a native pygame sprite but instead a pygame surface"""
-
-    def __init__(self, radius=50, color=(0, 0, 255), x=320, y=240):
-        """create a (black) surface and paint a blue ball on it"""
-        self.radius = radius
+    def __init__(self, color, starting_x, starting_y, vel):
+        self.vel = vel
+        self.x = starting_x
+        self.y = starting_y
         self.color = color
-        self.x = x
-        self.y = y
-        # create a rectangular surface for the ball 50x50
-        self.surface = pygame.Surface((2 * self.radius, 2 * self.radius))
-        # pygame.draw.circle(Surface, color, pos, radius, width=0) # from pygame documentation
-        pygame.draw.circle(self.surface, color, (radius, radius), radius)  # draw blue filled circle on ball surface
-        self.surface = self.surface.convert()  # for faster blitting.
-        # to avoid the black background, make black the transparent color:
-        # self.surface.set_colorkey((0,0,0))
-        # self.surface = self.surface.convert_alpha() # faster blitting with transparent color
+        self.direction = ''
 
-    def blit(self, background):
-        """blit the Ball on the background"""
-        background.blit(self.surface, (self.x, self.y))
+    def move(self):
+        if self.direction == 'L':
+            self.x -= self.vel
+        elif self.direction == 'R':
+            self.x += self.vel
+        elif self.direction == 'U':
+            self.y -= self.vel
+        elif self.direction == 'D':
+            self.y += self.vel
 
-
-####
 
 if __name__ == '__main__':
     # call with width of window and fps
-    PygView().run()
+    Game().run()
